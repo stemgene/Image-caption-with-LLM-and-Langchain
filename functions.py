57d2114310cbe0affb6783ed91233b7a2467dd1a
Haoyuan Dong <hlj12530@gmail.com>
2024-01-04 01:02:22 -0500

    finish functions

--- functions.py
+++ functions.py
@@ -1,12 +1,67 @@
+from transformers import BlipProcessor, BlipForConditionalGeneration, DetrImageProcessor, DetrForObjectDetection
+from PIL import Image
+import torch
+
 def get_image_caption(image_path):
-    # generate short captions for the provided image
-    pass
+    """
+    generate short captions for the provided image
+    
+    Args:
+        image_path (str): The path to the image file
+    
+    Returns:
+        str: A string representing the caption for the image
+    
+    """
+    image = Image.open(image_path).convert("RGB")
+
+    model_name = "Salesforce/blip-image-captioning-large"
+    device = "cuda" if torch.cuda.is_available() else 'cpu' # cuda
+    processor = BlipProcessor.from_pretrained(model_name)
+    model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
+
+    inputs = processor(image, return_tensors='pt').to(device)
+    output = model.generate(**inputs, max_new_tokens=20)
+
+    caption = processor.decode(output[0], skip_special_tokens=True)
+
+    return caption
+
 
 
 def detect_objects(image_path):
     # Detect the objects in the given image
-    pass
+    image = Image.open(image_path).convert("RGB")
+    device = "cuda" if torch.cuda.is_available() else 'cpu' # cuda
+
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+
+    inputs = processor(images=image, return_tensors='pt').to(device)
+    outputs = model(**inputs)
+
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.9
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+    detections = ""
+    for score, label, box in zip(results['scores'], results['labels'], results['boxes']):
+        # box = [round(i, 2) for i in box.tolist()]
+        # print(
+        #     f"Detected {model.config.id2label[label.item()]} with confidence "
+        #     f"{round(score.item(), 3)} at location {box}"
+        # )
+        detections += '[{}, {}, {}, {}]'.format(int(box[0]), int(box[1]), int(box[2]), int(box[3]))
+        detections += ' {}'.format(model.config.id2label[int(label)])
+        detections += ' {}\n'.format(float(score))
+    
+    return detections
 
 
 if __name__ == '__main__':
-    pass
+    image_path = r"C:\Users\hydon\Downloads\horseback_riding.jpg"
+    caption = get_image_caption(image_path)
+    print(caption)
+    detections = detect_objects(image_path)
+    print(detections)
--- main.py
+++ main.py
@@ -11,17 +11,20 @@ st.title("Ask a question to an image")
 st.header("Please upload an image")
 
 # upload file
-file = st.file_uploader("", types=['jpg', 'png', 'jpeg'])
+file = st.file_uploader("", type=['jpg', 'png', 'jpeg'])
 
 if file:
 
     # display image
-    st.image(file, use_column)
+    st.image(file, use_column_width=True)
 
     # text input
+    user_question = st.text_input('Ask a question your image:')
 
     ##############################
     ### Compute agent response ###
     ##############################
 
     # write agent response
+    if user_question and user_question != "":
+        st.write("dummy response")
